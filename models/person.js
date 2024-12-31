c6922bf10ff7eab3cc31ba159077301606f1b23d
hiepnguyenduc2005 <hiepnguyenduc2005@gmail.com>
2024-12-31 00:31:51 -0500

    uibuild

--- models/person.js
+++ models/person.js
@@ -22,7 +22,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 9,
     validate: {
-      alidator: function(v) {
+      validator: function(v) {
         return /^\d{2,3}-\d{5,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
