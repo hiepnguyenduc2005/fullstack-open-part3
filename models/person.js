e5ae22ea0304c858bd253c0dd78960280b708998
hiepnguyenduc2005 <hiepnguyenduc2005@gmail.com>
2024-12-31 00:28:07 -0500

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const url = process.env.MONGODB_URI;
-mongoose.set('strictQuery',false);
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery',false)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB: ', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -22,23 +22,23 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 9,
     validate: {
-        validator: function(v) {
-          return /^\d{2,3}-\d{5,}$/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
+      alidator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
     required: true,
   }
-});
-
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
 })
 
-const Person = mongoose.model('Person', personSchema);
-export default Person;
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+export default Person
 
--- mongo.js
+++ mongo.js
@@ -17,21 +17,21 @@ const personSchema = new mongoose.Schema({
 })
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length == 3) {
+if (process.argv.length === 3) {
   console.log('phonebook:')
   Person.find({}).then(result => {
     result.forEach(person => {
-        console.log(person.name, person.number)
+      console.log(person.name, person.number)
     })
     mongoose.connection.close()
-})
-}  
+  })
+}
 else {
   const person = new Person({
     name: process.argv[3],
     number: process.argv[4],
   })
-  person.save().then(result => {
+  person.save().then(() => {
     console.log('added', person.name, person.number, 'to phonebook')
     mongoose.connection.close()
   })
